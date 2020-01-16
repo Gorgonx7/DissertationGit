297953993d2a92d0aa1edeb7742fa820913a764a
James Gordon <james.gordon1628@gmail.com>
2020-01-16 18:26:11 +0000

    uploading object seen test

---
+++ ObjectSeenTest/Assets/SaveFileLoadingSystem/SerialisableComponent.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using UnityEngine;
+namespace ACE.FileSystem
+{
+    /// <summary>
+    ///  Momento class for any custom gameobject within the system
+    /// </summary>
+    [Serializable]
+    public class SerialisableComponent<T> : ISerializableComponent where T : Component
+    {
+        //The concists of everything that a custom object may need to be serialised
+        //This specific schema has no data on position, rotation, only graphical and mechanical 
+        [XmlAttribute("Component")]
+        private T m_Component;
+        [XmlAttribute("ComponentType")]
+        Type componentType;
+        Dictionary<string, ACE_PropertyField> componentVariable = new Dictionary<string, ACE_PropertyField>();
+        public SerialisableComponent(T pComponent)
+        {
+            m_Component = pComponent;
+            componentType = pComponent.GetType();
+        }
+        public SerialisableComponent()
+        {
+            componentType = typeof(T);
+        }
+        public void SetComponent(T pComponent)
+        {
+            m_Component = pComponent;
+        }
+        public T GetComponent()
+        {
+            return m_Component;
+        }
+        public new Type GetType()
+        {
+            return componentType;
+        }
+        public void addToGameObject(GameObject objectToAttatchTo)
+        {
+            Component comp = objectToAttatchTo.AddComponent(componentType);
+            foreach (KeyValuePair<String, ACE_PropertyField> i in componentVariable)
+            {
+                object value = i.Value.m_Val;
+                if (i.Value.m_type.Contains("UnityEngine"))
+                {
+                    string typeVal = i.Value.m_type;
+                    if (typeVal == "UnityEngine.Mesh")
+                    {
+                        value = Resources.Load<Mesh>(i.Value.m_Val);
+                    }
+                }
+                else
+                {
+                    componentType.GetProperty(i.Key).SetValue(comp, value);
+                }
+                comp = m_Component;
+
+            }
+        }
+
+            public Type GetSerializedType()
+            {
+                return componentType;
+            }
+
+            public XmlSchema GetSchema()
+            {
+                return null;
+            }
+
+            public SerialisableComponent<Component> ReadXml(XmlReader reader)
+            {
+                SerialisableComponent<Component> Output = new SerialisableComponent<Component>();
+                if (reader.AttributeCount > 0)
+                {
+                    string type = reader.GetAttribute(0);
+                    if (type.Contains("UnityEngine"))
+                    {
+                        type = type + ", UnityEngine";
+                    }
+                    componentType = Type.GetType(type);
+
+                    Output.componentType = this.componentType;
+
+                }
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (reader.Name == componentType.ToString())
+                            {
+                                while (reader.MoveToNextAttribute())
+                                {
+                                    componentVariable.Add(reader.Name, new ACE_PropertyField(componentType.GetProperty(reader.Name).PropertyType.Name, reader.Value));
+                                }
+                            }
+                            break;
+                        case XmlNodeType.Attribute:
+                            if (reader.Name == "Type")
+                            {
+                                componentType = Type.GetType(reader.Value);
+                            }
+                            break;
+
+                        case XmlNodeType.EndElement:
+                            if (reader.Name == "Component")
+                            {
+                                return Output;
+                            }
+                            break;
+                    }
+                }
+                return Output;
+            }
+
+            public void WriteXml(XmlWriter writer)
+            {
+                writer.WriteStartElement("Component");
+                writer.WriteAttributeString("Type", componentType.ToString());
+                writer.WriteStartElement(componentType.ToString());
+                PropertyInfo[] info = componentType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+
+                foreach (PropertyInfo i in info)
+                {
+                    if (i.CanWrite)
+                    {
+                        if (i.GetValue(m_Component) != null)
+                        {
+                            writer.WriteAttributeString(i.Name, i.GetValue(m_Component).ToString());
+                        }
+                    }
+                }
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+            }
+
+
+        }
+  }
+
+public struct ACE_PropertyField
+{
+
+    public string m_Val
+    {
+        get
+        {
+            return m_Val;
+        }
+        set
+        {
+            m_Val = value;
+        }
+    }
+    public string m_type
+    {
+        get
+        {
+            return m_type;
+        }
+        set
+        {
+            m_type = value;
+        }
+    }
+    public ACE_PropertyField(string type, string valuePair)
+    {
+        m_Val = valuePair;
+        m_type = type;
+    }
+}
---
+++ ObjectSeenTest/Assets/Test_Suite/CanSaveDefaultGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ACE.FileSystem;
+public class CanSaveDefaultGameObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject TestObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObjectCaretaker caretaker = new GameObjectCaretaker();
+        caretaker.SaveObject(TestObject);
+        //Load the object back in
+        caretaker.LoadObject("test");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
